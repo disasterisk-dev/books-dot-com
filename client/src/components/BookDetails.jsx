5f78d41ae21c7dedc41e36e78c1888b546fa77c3
JoelRobinsonUK <joelsgrobinson@gmail.com>
2023-12-05 17:08:44 +0000

    Update BookDetails.jsx

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -23,29 +23,31 @@ const BookDetails = ({ book }) => {
     }, [book.finished, book.rating]);
 
     return (
-        <Link to={`/books/${book._id}`}>
-            <div
-                className={`flex items-center rounded-lg border-l-8 bg-white p-3 ${fColor}`}
-            >
-                <div className="grow">
-                    <h4 className="text-2xl">
-                        <strong>{book.title}</strong>
-                    </h4>
-                    <p>{book.author}</p>
+        <>
+            <Link to={`/books/${book._id}`}>
+                <div
+                    className={`flex items-center rounded-lg border-l-8 bg-white p-3 ${fColor}`}
+                >
+                    <div className="grow">
+                        <h4 className="text-2xl">
+                            <strong>{book.title}</strong>
+                        </h4>
+                        <p>{book.author}</p>
+                    </div>
+                    <Rating
+                        readOnly
+                        size="large"
+                        precision={0.5}
+                        value={book.rating}
+                    />
                 </div>
-                <Rating
-                    readOnly
-                    size="large"
-                    precision={0.5}
-                    value={book.rating}
-                />
-            </div>
-            <span className="mb-4 block pr-2 pt-1 text-right text-xs">
+            </Link>
+            <span className="mb-4 block select-none pr-2 pt-1 text-right text-xs">
                 {formatDistanceToNow(new Date(book.createdAt), {
                     addSuffix: true,
                 })}
             </span>
-        </Link>
+        </>
     );
 };
 
